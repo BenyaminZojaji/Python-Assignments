2da9aac2a722a8fe03ad33e957f9a72370e74893
Benyamin Zojaji <77120507+BenyaminZojaji@users.noreply.github.com>
2021-09-24 12:42:51 +0330

    Update main.py

--- Assignment14/T-rex runner/main.py
+++ Assignment14/T-rex runner/main.py
@@ -16,53 +16,44 @@ class Game(arcade.Window):
         self.backeground_day_image = arcade.load_texture("images/background_day.png")
         self.gameOver = False
         self.night = True
-
         self.cactus_speed = -4
         self.pterodactyl_speed = -5
-
         self.time_score = time.time()
         self.time_cactus = time.time()
         self.time_pterodactyl = time.time()
         self.time_backgroundChange = time.time()
-
         self.cactus_list = arcade.SpriteList()
         self.pterodactyl_list = arcade.SpriteList()
-
         self.dino = Dino()
-
         self.moon = arcade.Sprite('images/moon.png')
         self.moon.center_x = self.w
         self.moon.center_y = random.randint(400, 600)
         self.moon.change_x = -0.5
         self.moon.change_y = 0
-
         self.star = arcade.Sprite('images/star.png')
         self.star.center_x = self.w
         self.star.center_y = random.randint(250, 750)
         self.star.change_x = -1
         self.star.change_y = 0
-        
         self.cloud = arcade.Sprite('images/cloud_night.png')
         self.cloud.center_x = self.w
         self.cloud.center_y = random.randint(350, 750)
         self.cloud.change_x = -1.5
         self.cloud.change_y = 0
-
         self.ground_list = arcade.SpriteList()
         for i in range(0, self.w, 64):
             ground = Ground(i ,100)
             self.ground_list.append(ground)
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.dino, self.ground_list, self.gravity)
-
     def on_draw(self):
         self.time_now = time.time()
         if self.gameOver:
             if self.night:
-                arcade.draw_text('YOU Lose', self.w//2-200, self.h//2, arcade.color.WHITE, DEFAULT_FONT_SIZE, width=400, align='center')
-                arcade.draw_text('Press R for reset game', self.w//2-200, self.h//2-100, arcade.color.WHITE, DEFAULT_FONT_SIZE//2, width=400, align='center')
+                arcade.draw_text('YOU Lose', self.w//2-200, self.h//2, arcade.color.WHITE, DEFAULT_FONT_SIZE, width=400, font_name='Kenney Mini Square', align='center')
+                arcade.draw_text('Press R for reset game', self.w//2-200, self.h//2-100, arcade.color.WHITE, DEFAULT_FONT_SIZE//2, width=400, font_name='Kenney Mini Square', align='center')
             else:
-                arcade.draw_text('YOU Lose', self.w//2-200, self.h//2, arcade.color.BLACK, DEFAULT_FONT_SIZE, width=400, align='center')
-                arcade.draw_text('Press R for reset game', self.w//2-200, self.h//2-100, arcade.color.BLACK, DEFAULT_FONT_SIZE//2, width=400, align='center')
+                arcade.draw_text('YOU Lose', self.w//2-200, self.h//2, arcade.color.BLACK, DEFAULT_FONT_SIZE, width=400, font_name='Kenney Mini Square', align='center')
+                arcade.draw_text('Press R for reset game', self.w//2-200, self.h//2-100, arcade.color.BLACK, DEFAULT_FONT_SIZE//2, width=400, font_name='Kenney Mini Square', align='center')
         else:
             arcade.start_render()
             if self.time_now - self.time_backgroundChange > 15:
@@ -86,9 +77,9 @@ class Game(arcade.Window):
             self.star.draw()
             self.dino.draw()
             if self.night:
-                arcade.draw_text('HI  %s  %s' %(str(self.dino.highScore).zfill(5), str(self.dino.score).zfill(5)), self.w-220, self.h-30, arcade.color.WHITE, DEFAULT_FONT_SIZE//2, width=400, align='left')
+                arcade.draw_text('HI  %s  %s' %(str(self.dino.highScore).zfill(5), str(self.dino.score).zfill(5)), self.w-220, self.h-30, arcade.color.WHITE, DEFAULT_FONT_SIZE//2, width=400, font_name='Kenney Mini Square', align='left')
             else:
-                arcade.draw_text('HI  %s  %s' %(str(self.dino.highScore).zfill(5), str(self.dino.score).zfill(5)), self.w-220, self.h-30, arcade.color.BLACK, DEFAULT_FONT_SIZE//2, width=400, align='left')
+                arcade.draw_text('HI  %s  %s' %(str(self.dino.highScore).zfill(5), str(self.dino.score).zfill(5)), self.w-220, self.h-30, arcade.color.BLACK, DEFAULT_FONT_SIZE//2, width=400, font_name='Kenney Mini Square', align='left')
     def on_update(self, delta_time: float):
         self.time_now = time.time()
         if not self.gameOver:
@@ -100,8 +91,11 @@ class Game(arcade.Window):
         self.physics_engine.update()
         for pterodactyl in self.pterodactyl_list:
             pterodactyl.update_animation()
-        if self.dino.time_startDown + 0.5 < self.time_now:
+        if not self.dino.down:
             self.dino.update_animation()
+        else:
+            self.dino.manual_walkDown_animation()
+            self.dino.down_frame += 1
         for ground in self.ground_list:
             if ground.center_x<0:
                 self.ground_list.remove(ground)
@@ -136,7 +130,6 @@ class Game(arcade.Window):
         self.star.update()
         self.cloud.update()
         self.dino.set_x()
-
         for cactus in self.cactus_list:
             if arcade.check_for_collision(self.dino, cactus):
                 self.gameOver = True
@@ -149,9 +142,7 @@ class Game(arcade.Window):
                 self.dino.change_y = 12
                 self.dino.jump_sound()
         elif key==arcade.key.DOWN:
-            self.dino.texture = arcade.load_texture('images/down0.png')
             self.dino.down = True
-            self.dino.time_startDown = time.time()
         elif key==arcade.key.R and self.gameOver:
             self.gameOver = False
             if self.dino.score > self.dino.highScore:
@@ -161,7 +152,9 @@ class Game(arcade.Window):
             self.time_score = time.time()
             self.cactus_speed = -4
             self.pterodactyl_speed = -5
-
+    def on_key_release(self, key, modifiers):
+        if key==arcade.key.DOWN:
+            self.dino.down = False
 if __name__ == '__main__':
     game = Game()
-    arcade.run()   
+    arcade.run()
